e89fa06d254b618cb3d9785946530f29233d49f9
Akex Mac <1115220867@qq.com>
2023-12-17 21:48:57 +0800

    重写调用gurobi

--- gl_gurobi.py
+++ gl_gurobi.py
@@ -5,34 +5,32 @@ import os
 import utils
 from utils import logger
 
-def solver_gurobi(x0, A, b, mu, opts={}):
+def gl_gurobi(x0: np.ndarray, A: np.ndarray, b: np.ndarray, mu: float, opts={}):
     m, n = A.shape
     l = b.shape[1]
-    # print('Note: for gurobi, x0 is ignored')
-    # t1 = time.time_ns()
     model = gp.Model()
-    # t2 = time.time_ns()
     X = model.addMVar((n, l), lb=-gp.GRB.INFINITY)
+    # X = model.addMVar((n, l), lb=-gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="x")
     X.start = x0
     Y = model.addMVar((m, l), lb=-gp.GRB.INFINITY)
     ts = model.addMVar(n)
-    # t3 = time.time_ns()
     for j in range(l):
         model.addConstr(A @ X[:, j] - b[:, j] == Y[:, j])
-    # t4 = time.time_ns()
     for i in range(n):
-        # model.addConstr(X[i, :] @ X[i, :] <= ts[i] @ ts[i])
         model.addConstr(X[i, :] @ X[i, :] <= ts[i] * ts[i])
-    # t5 = time.time_ns()
-    # print(t2 - t1, t3 - t2, t4 - t3, t5 - t4)
     model.setObjective(0.5 * sum([Y[:, j] @ Y[:, j] for j in range(l)]) + mu * sum(ts), gp.GRB.MINIMIZE)
     model.optimize()
-    outs = {}
-    with open(r'./logs/gl_cvx.log', encoding='utf-8') as f:
-        outs['output'] = f.read()
-    logger.info(f"output: {outs['output']}")
-    iters = utils.parse_iters(outs['output'], 'GUROBI')
-    logger.info(f"iters: {iters}")
-    return model.objVal, X.x, len(iters), {'iters': iters}
+    with open('./logs/gl_cvx.log', 'r', encoding='utf-8') as f:
+        logs = f.read()
+    iters = utils.parse_iters(logs, 'GUROBI')
 
-solvers = {'gurobi_SOCP': solver_gurobi}
+    utils.logger.info(f"#######==Solver: cvx(MOSEK)==#######")
+    utils.logger.info(f"Objective value: {model.objVal}")
+    utils.logger.info(f"Status: {model.status}")
+    # utils.logger.info(f"Solver status: {model.solver_stats}")
+    utils.logger.info(f"#######==CVXPY's Logs:==#######\n{logs}")
+    utils.logger.info(f"#######==END of Logs:==#######")
+    utils.logger.info(f"iters after parse:\n{iters}")
+    # 最优解，迭代次数，{iters每次迭代目标函数值情况，cpu_time求解时间；obj目标函数值}
+    out = {'iters': iters, 'obj': model.objVal}
+    return X.x, len(iters), out
